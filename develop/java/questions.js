cd65cb4cc5d28f45baaceabd1f428a09c3e88138
Mike Thomas <mthomas.theadvocate@gmail.com>
2021-08-22 10:18:54 -0700

    Java

---
+++ develop/java/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+   {
+       title: "Commonly used data types DO NOT include",
+       choices: ["strings, boolean", "alerts", "numbers"],
+       answers: ["alerts"]  
+   },
+
+
+   {
+    title: "Commonly used data types DO NOT include",
+    choices: ["strings, boolean", "alerts", "numbers"],
+    answers: ["alerts"]  
+   },
+   
+   {
+    title: "Commonly used data types DO NOT include",
+    choices: ["strings, boolean", "alerts", "numbers"],
+    answers: ["alerts"]  
+   },
+
+   {
+    title: "Commonly used data types DO NOT include",
+    choices: ["strings, boolean", "alerts", "numbers"],
+    answers: ["alerts"]  
+   },
+
+   {
+    title: "Commonly used data types DO NOT include",
+    choices: ["strings, boolean", "alerts", "numbers"],
+    answers: ["alerts"]  
+   }
+
+];
